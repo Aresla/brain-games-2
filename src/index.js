419a189307e6d1af64128c3c8ad247fb559150b8
Aresla <areslas@list.ru>
2023-11-09 10:25:01 +0200

    renew react version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import 'antd/dist/antd.css';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {App} from "./App";
+import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <App />
+  </React.StrictMode>
 );
+
 
 reportWebVitals(console.log);
